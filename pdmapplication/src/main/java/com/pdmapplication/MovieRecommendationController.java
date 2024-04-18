35e3e0586aba6aef097777435175058fa8296746
Jonathan Zhu <66046808+Jonathanz4344@users.noreply.github.com>
2024-04-18 15:08:17 -0400

    vbox change

--- pdmapplication/src/main/java/com/pdmapplication/MovieRecommendationController.java
+++ pdmapplication/src/main/java/com/pdmapplication/MovieRecommendationController.java
@@ -2,9 +2,18 @@ package com.pdmapplication;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
-import javafx.scene.control.ListView;
+import javafx.scene.control.Label;
 import javafx.scene.control.TabPane;
+import javafx.scene.layout.VBox;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -16,17 +25,22 @@ public class MovieRecommendationController {
     @FXML
     private TabPane tabPane;
 
-    @FXML
-    private ListView<String> top20MostPopularLast90DaysListView;
+    private VBox top20MostPopularLast90DaysListView;
+    private VBox top20MostPopularFollowersListView;
+    private VBox top5LatestReleasesListView;
+    private VBox recommendationsForYouListView;
 
-    @FXML
-    private ListView<String> top20MostPopularFollowersListView;
+    // @FXML
+    // private ListView<String> top20MostPopularLast90DaysListView;
 
-    @FXML
-    private ListView<String> top5LatestReleasesListView;
+    // @FXML
+    // private ListView<String> top20MostPopularFollowersListView;
 
-    @FXML
-    private ListView<String> recommendationsForYouListView;
+    // @FXML
+    // private ListView<String> top5LatestReleasesListView;
+
+    // @FXML
+    // private ListView<String> recommendationsForYouListView;
 
     private Connection connection;
 
@@ -37,6 +51,8 @@ public class MovieRecommendationController {
 
     @FXML
     public void initialize() {
+        showAlert(Alert.AlertType.ERROR, "ResultSet", "start");
+        System.out.println("working");
         loadTop20MostPopularLast90Days();
         loadTop20MostPopularFollowers();
         loadTop5LatestReleases();
@@ -44,26 +60,39 @@ public class MovieRecommendationController {
     }
 
     private void loadTop20MostPopularLast90Days() {
+        
         try {
             // Query for the top 20 most popular movies in the last 90 days
+            showAlert(Alert.AlertType.ERROR, "ResultSet", "1");
             String query = "SELECT m.movie_id, m.title, r.release_year " +
                            "FROM movie m JOIN released r ON m.movie_id = r.movie_id " +
                            "ORDER BY r.release_year DESC LIMIT 20;";
+            showAlert(Alert.AlertType.ERROR, "ResultSet", "2");
+            System.out.println("Print query test 1");
             PreparedStatement statement = connection.prepareStatement(query);
+            showAlert(Alert.AlertType.ERROR, "ResultSet", "3");
             ResultSet resultSet = statement.executeQuery();
+            showAlert(Alert.AlertType.ERROR, "ResultSet", "4");
+            System.out.println("result set: " + resultSet);
+            showAlert(Alert.AlertType.ERROR, "ResultSet", "wokign");
 
             // Display the result in the ListView
-            top20MostPopularLast90DaysListView.getItems().clear();
+            // top20MostPopularLast90DaysListView.getChildren().clear();
 
 
             while (resultSet.next()) {
                 String title = resultSet.getString("title");
-                top20MostPopularLast90DaysListView.getItems().add(title);
+                showAlert(Alert.AlertType.ERROR, "ResultSet", "loop");
+                Label movieLabel = new Label(title);
+                showAlert(Alert.AlertType.ERROR, "ResultSet", "add");
+                top20MostPopularLast90DaysListView.getChildren().add(movieLabel);
+                showAlert(Alert.AlertType.ERROR, "ResultSet", "afteradd");
+                
+                System.out.println(title);
             }
 
-            statement.close();
-            resultSet.close();
-                
+            // statement.close();
+            // resultSet.close();     
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -86,14 +115,15 @@ public class MovieRecommendationController {
             ResultSet resultSet = statement.executeQuery();
 
             // Display the result in the ListView
-            top20MostPopularFollowersListView.getItems().clear();
+            top20MostPopularFollowersListView.getChildren().clear();
             while (resultSet.next()) {
                 String title = resultSet.getString("title");
-                top20MostPopularFollowersListView.getItems().add(title);
+                Label movieLabel = new Label(title);
+                top20MostPopularFollowersListView.getChildren().add(movieLabel);
             }
 
-            statement.close();
-            resultSet.close();
+            // statement.close();
+            // resultSet.close();
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -111,14 +141,15 @@ public class MovieRecommendationController {
             ResultSet resultSet = statement.executeQuery();
 
             // Display the result in the ListView
-            top5LatestReleasesListView.getItems().clear();
+            top5LatestReleasesListView.getChildren().clear();
             while (resultSet.next()) {
                 String title = resultSet.getString("title");
-                top5LatestReleasesListView.getItems().add(title);
+                Label movieLabel = new Label(title);
+                top5LatestReleasesListView.getChildren().add(movieLabel);
             }
 
-            statement.close();
-            resultSet.close();
+            // statement.close();
+            // resultSet.close();
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -141,14 +172,15 @@ public class MovieRecommendationController {
             ResultSet resultSet = statement.executeQuery();
 
             // Display the result in the ListView
-            recommendationsForYouListView.getItems().clear();
+            recommendationsForYouListView.getChildren().clear();
             while (resultSet.next()) {
                 String title = resultSet.getString("title");
-                recommendationsForYouListView.getItems().add(title);
+                Label movieLabel = new Label(title);
+                recommendationsForYouListView.getChildren().add(movieLabel);
             }
 
-            statement.close();
-            resultSet.close();
+            // statement.close();
+            // resultSet.close();
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -157,6 +189,8 @@ public class MovieRecommendationController {
 
     
     }
+
+
 
     private void showAlert(Alert.AlertType type, String title, String content) {
         Alert alert = new Alert(type);
